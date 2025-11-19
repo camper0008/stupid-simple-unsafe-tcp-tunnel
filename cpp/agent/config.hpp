44fdf1180bb54c07681cac96d7a4eb70e04427b4
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-11-19 08:48:22 +0100

    c -> cpp

---
+++ cpp/agent/config.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Connection {
+  std::string hostname;
+  std::uint16_t port;
+  std::string secret;
+};
+
+struct Config {
+  std::vector<Connection> connections;
+};
+
+static Config global_conf{{{"localhost", 4200, "abcd"}}};
---
+++ cpp/agent/main.cpp
@@ -0,0 +1,5 @@
+#include "config.hpp"
+
+auto handle_thread(const Connection& connection) -> void {}
+
+int main() { for (const auto &connection : global_conf.connections){} }
